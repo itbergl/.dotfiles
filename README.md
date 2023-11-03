0ba765c462693989f225cbcd397986017e71f2f9
itbergl <76241103+itbergl@users.noreply.github.com>
2023-11-04 00:58:38 +0800

    added auto-installers for CLI tools

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # .dotfiles
+
+## Neovim Config
+
+This is a repo for my Neovim configuration files. I use Lazy as a plugin manager.
+
+I am planning on making a new branch for a more "barebones" config for quick use on new machines.
--- install.sh
+++ install.sh
@@ -3,5 +3,48 @@
 DOTFILES=$HOME/.dotfiles
 
 # create sym links
-ln -sf $DOTFILES/.zshrc $HOME/.zshrc
-ln -sf $DOTFILES/.tmux.conf $HOME/.tmux.conf
+echo "Creating sym links"
+ln -sf $DOTFILES/.zshrc $HOME/.zshrc 2>/dev/null
+ln -sf $DOTFILES/.tmux.conf $HOME/.tmux.conf 2>/dev/null
+ln -sd $DOTFILES/nvim $HOME/.config/nvim 2>/dev/null
+
+packages=("fzf" "ripgrep" "bat")
+
+echo "Installing packages"
+for package in "${packages[@]}"; do
+    if dpkg -l | grep -q "$package"; then
+        echo "$package is already installed"
+    else
+        echo "installing $package"
+        sudo apt-get install -y "$package"
+        if [ $? -eq 0 ]; then
+            echo "$package has been successfully installed"
+        else 
+            echo "Failed to install $package"
+        fi
+    fi
+done
+
+# specail packages
+if ! dpkg -l | grep -q exa; then
+    echo "installing exa"
+    exa_package_name="exa_0.9.0-4_amd64.deb"
+    wget -c http://old-releases.ubuntu.com/ubuntu/pool/universe/r/rust-exa/$exa_package_name
+    sudo apt-get install ./$exa_package_name
+    rm $exa_package_name
+else 
+    echo "exa is already installed"
+fi
+
+if ! [ -e /usr/local/bin/lazygit ]; then
+    echo "installing lazygit"
+    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+    tar xf lazygit.tar.gz lazygit
+    sudo install lazygit /usr/local/bin
+    rm lazygit.tar.gz
+    rm lazygit
+else 
+    echo "lazygit is already installed"
+fi
+
